a12b75978f39a7a3329e171fe59453322425e3f7
RinLit <lintusup@foxmail.com>
2024-11-09 22:21:27 +0800

    fix: again x2) the bug about detection of check window fullscreen #114

--- main.py
+++ main.py
@@ -305,6 +305,11 @@ def check_fullscreen():  # 检查是否全屏
     user32.GetWindowRect(user32.GetDesktopWindow(), ctypes.byref(desktop_rect))
     # 获取当前窗口的矩形
     app_rect = RECT()
+    title_buffer = ctypes.create_unicode_buffer(256)
+    user32.GetWindowTextW(hwnd, title_buffer, 256)
+    if title_buffer.value == "Application Frame Host":
+        print('test')
+        return False
     user32.GetWindowRect(hwnd, ctypes.byref(app_rect))
     if hwnd == user32.GetDesktopWindow():
         return False
